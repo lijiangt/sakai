7d6a5cb92e9066ba2cbaf592092afdc17a6b43a6
lijiangt <lijiangt@gmail.com>
2011-08-19 01:36:29 +0000

    modify name

--- sakai-mini/2.8.0/reference/docs/samigo_template_translate.sql
+++ sakai-mini/2.8.0/reference/docs/samigo_template_translate.sql
@@ -1,7 +1,12 @@
-update SAM_ASSESSMENTBASE_T set TITLE='��ѧ����',DESCRIPTION='ϵͳĬ�϶���Ĳ�������' where TITLE='Formative Assessment';
-update SAM_ASSESSMENTBASE_T set TITLE='�⼯',DESCRIPTION='ϵͳĬ�϶���Ĳ�������' where TITLE='Problem Set';
-update SAM_ASSESSMENTBASE_T set TITLE='��β���',DESCRIPTION='ϵͳĬ�϶���Ĳ�������' where TITLE='Quiz';
-update SAM_ASSESSMENTBASE_T set TITLE='����',DESCRIPTION='ϵͳĬ�϶���Ĳ�������' where TITLE='Survey';
-update SAM_ASSESSMENTBASE_T set TITLE='����',DESCRIPTION='ϵͳĬ�϶���Ĳ�������' where TITLE='Test';
-update SAM_ASSESSMENTBASE_T set TITLE='��ʱ����',DESCRIPTION='ϵͳĬ�϶���Ĳ�������' where TITLE='Timed Test';
-update SAM_ASSESSMENTBASE_T set TITLE='Ĭ�ϲ�������',DESCRIPTION='ϵͳĬ�϶���Ĳ�������' where TITLE='Default Assessment Type';	
+update SAM_ASSESSMENTBASE_T set TITLE='教学评测',DESCRIPTION='系统默认定义的测验类型' where TITLE='Formative Assessment';
+update SAM_ASSESSMENTBASE_T set TITLE='习题集',DESCRIPTION='系统默认定义的测验类型' where TITLE='Problem Set';
+update SAM_ASSESSMENTBASE_T set TITLE='随堂测验',DESCRIPTION='系统默认定义的测验类型' where TITLE='Quiz';
+update SAM_ASSESSMENTBASE_T set TITLE='调查',DESCRIPTION='系统默认定义的测验类型' where TITLE='Survey';
+update SAM_ASSESSMENTBASE_T set TITLE='测验',DESCRIPTION='系统默认定义的测验类型' where TITLE='Test';
+update SAM_ASSESSMENTBASE_T set TITLE='记时测验',DESCRIPTION='系统默认定义的测验类型' where TITLE='Timed Test';
+update SAM_ASSESSMENTBASE_T set TITLE='默认测验类型',DESCRIPTION='系统默认定义的测验类型' where TITLE='Default Assessment Type';	
+
+update SAM_ASSESSMENTBASE_T set TITLE='随堂测验',DESCRIPTION='系统默认定义的测验类型' where TITLE='随课测验';
+update SAM_ASSESSMENTBASE_T set TITLE='习题集',DESCRIPTION='系统默认定义的测验类型' where TITLE='题集';
+update SAM_ASSESSMENTBASE_T set TITLE='测验',DESCRIPTION='系统默认定义的测验类型' where TITLE='测试';
+update SAM_ASSESSMENTBASE_T set TITLE='记时测验',DESCRIPTION='系统默认定义的测验类型' where TITLE='即时测试';
